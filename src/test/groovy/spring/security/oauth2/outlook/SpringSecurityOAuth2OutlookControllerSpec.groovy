8d9c551fd510b642a1e62cd6b2299a68e58cd75b
Andre <andre.p23@hotmail.com>
2018-10-24 20:58:57 -0700

    Change: versión to 0.1

---
+++ src/test/groovy/spring/security/oauth2/outlook/SpringSecurityOAuth2OutlookControllerSpec.groovy
@@ -0,0 +1,18 @@
+package spring.security.oauth2.outlook
+
+import grails.testing.web.controllers.ControllerUnitTest
+import spock.lang.Specification
+
+class SpringSecurityOAuth2OutlookControllerSpec extends Specification implements ControllerUnitTest<SpringSecurityOAuth2OutlookController> {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
